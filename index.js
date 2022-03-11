be133c5923c7ff0d121d12b5a68d663463682c09
Rashedul Hasan <Rashedul Hasan>
2022-03-12 03:15:52 +0600

    JS-Class-02

---
+++ index.js
@@ -0,0 +1,45 @@
+// var firstName = 'Rashedul';
+// var lastName = 'Hasan';
+// var fullName = firstName+" "+lastName;
+// console.log(fullName);
+
+// var num = Math.abs(-23);
+// console.log(num);
+
+// var num2 = Math.ceil(23.345);
+// console.log(num2);
+
+// var num3 = Math.floor(23.3545);
+// console.log(num3);
+
+// var num4 = Math.round(.4999);
+// console.log(num4);
+
+// var num5 = Math.round(.5);
+// console.log(num5);
+
+// var num6 = Math.random()*100;
+// console.log(num6);
+
+
+
+// var mangoPrice = 10;
+// if(mangoPrice != 10){
+//     console.log("You can buy");
+// }
+// else{
+//     console.log("You can't buy");
+// }
+
+var friend1Taka = 100;
+var friend2Taka = 60;
+if(friend1Taka == 100 && friend2Taka == 50){
+    console.log("Go to Restaturant-01");
+}
+else if(friend1Taka>=100 || friend2Taka>=100){
+    console.log("Go to Restaturant-02");
+}
+else{
+    console.log("Can't go to Restaturant");
+}
+
